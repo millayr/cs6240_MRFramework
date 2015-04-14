bbb5a809bf0f0bfc2073a5777687d129e0504997
PramodKhare <kharepr@ccs.neu.edu>
2015-04-13 21:51:17 -0400

    Removed the Writable all together, added generics type casting into POCallback
    Incremental Dev Commit
    
    End-to-flow check remaining

--- src/main/java/com/neu/tests/mrlite/TestInterface.java
+++ src/main/java/com/neu/tests/mrlite/TestInterface.java
@@ -4,41 +4,40 @@ import java.io.FileNotFoundException;
 import java.io.UnsupportedEncodingException;
 import java.net.UnknownHostException;
 
-import com.neu.mrlite.Assortment;
-import com.neu.mrlite.IOHandle;
-import com.neu.mrlite.POCallback;
-import com.neu.mrlite.Pair;
-import com.neu.mrlite.Writable;
+import com.neu.mrlite.common.datastructures.Assortment;
+import com.neu.mrlite.common.datastructures.IOHandle;
+import com.neu.mrlite.common.datastructures.POCallback;
+import com.neu.mrlite.common.datastructures.Pair;
 
 public class TestInterface {
     public static Assortment run(String inFile, String outDir) throws FileNotFoundException, UnsupportedEncodingException, UnknownHostException {
 		
 		IOHandle io = new IOHandle(inFile, outDir);
 		
-		Assortment<String> col1 = Assortment.readInputFrom(io);
+		Assortment<Pair<Long, String>> col1 = Assortment.readInputFrom(io);
 		
 		Assortment<Pair<String, Integer>> col2 = col1.parallel(
-			new POCallback() {
-				public void process(Writable line) {
-					String[] parts = line.cast(String.class).split("\\t");
+			new POCallback<Long, String, String, Integer>() {
+				public void process(Pair<Long, String> data) {
+					String[] parts = data.getValue().split("\\t");
 					emit(parts[0], Integer.parseInt(parts[1]));
 				}
 			});
 		
 		Assortment<Pair<String, Integer>> col3 = col2.parallel(
-			new POCallback() {
-				public void process(Writable obj) {
-					int val = (Integer) obj.cast(Pair.class).value;
-					String key = (String) obj.cast(Pair.class).key;
+			new POCallback<String, Integer, String, Integer>() {
+				public void process(Pair<String, Integer> data) {
+					int val = data.getValue();
+					String key = data.getKey();
 					emit(key, val+10);
 				}
 			});
 		
 		Assortment<Pair<String, Integer>> col4 = col3.parallel(
-			new POCallback() {
-				public void process(Writable obj) {
-				    int val = (Integer) obj.cast(Pair.class).value;
-                    String key = (String) obj.cast(Pair.class).key;
+		        new POCallback<String, Integer, String, Integer>() {
+	                public void process(Pair<String, Integer> data) {
+					int val =data.getValue();
+					String key = data.getKey();
 					emit(key, val+10);
 				}
 			});
